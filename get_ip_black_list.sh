e2ebbb1d37cc687eb6c786af597fd131acf6dbb2
Mohammed Habib <mhabib1981@gmail.com>
2015-06-05 04:07:08 -0700

    update

--- get_ip_black_list.sh
+++ get_ip_black_list.sh
@@ -4,19 +4,19 @@ path=$(pwd)
 ts=$(date +"%d%m%y")
 
 #Zeus
-wget -q -O - https://zeustracker.abuse.ch/blocklist.php?download=ipblocklist --no-check-certificate | egrep -v '(^[[:space:]]*/|^[[:space:]]*#|^[[:space:]]*$)|/[0-9]|\:|/g' | awk -F" " '{print $1}' | sort -u >> $path/$ts'_$ts'_ip_blacklist.txt''
+wget -q -O - https://zeustracker.abuse.ch/blocklist.php?download=ipblocklist --no-check-certificate | egrep -v '(^[[:space:]]*/|^[[:space:]]*#|^[[:space:]]*$)|/[0-9]|\:|/g' | awk -F" " '{print $1}' | sort -u >> $path/$ts_'ip_blacklist.txt'
 
 #Spyeye
-wget -q -O - https://spyeyetracker.abuse.ch/blocklist.php?download=ipblocklist --no-check-certificate | egrep -v '(^[[:space:]]*/|^[[:space:]]*#|^[[:space:]]*$)|/[0-9]|\:|/g' | awk -F" " '{print $1}' | sort -u >> $path/$ts'_$ts'_ip_blacklist.txt''
+wget -q -O - https://spyeyetracker.abuse.ch/blocklist.php?download=ipblocklist --no-check-certificate | egrep -v '(^[[:space:]]*/|^[[:space:]]*#|^[[:space:]]*$)|/[0-9]|\:|/g' | awk -F" " '{print $1}' | sort -u >> $path/$ts_'ip_blacklist.txt'
 
 #OpenBl
-wget -q -O - http://www.openbl.org/lists/base.txt | egrep -v '(^[[:space:]]*/|^[[:space:]]*#|^[[:space:]]*$)|/[0-9]|\:|/g' | awk -F" " '{print $1}' | sort -u >> $path/$ts'_$ts'_ip_blacklist.txt''
+wget -q -O - http://www.openbl.org/lists/base.txt | egrep -v '(^[[:space:]]*/|^[[:space:]]*#|^[[:space:]]*$)|/[0-9]|\:|/g' | awk -F" " '{print $1}' | sort -u >> $path/$ts_'ip_blacklist.txt'
 
 #Palevo
 wget -q -O - https://palevotracker.abuse.ch/blocklists.php?download=ipblocklist --no-check-certificate | egrep -v '(^[[:space:]]*/|^[[:space:]]*#|^[[:space:]]*$)|/[0-9]|\:|/g' | awk -F" " '{print $1}' | sort -u >> $path/$ts'_ip_blacklist.txt'
 
 #Malc0de
-wget -q -O - http://malc0de.com/bl/$ts'_ip_blacklist.txt' | egrep -v '(^[[:space:]]*/|^[[:space:]]*#|^[[:space:]]*$)|/[0-9]|\:|/g' | awk -F" " '{print $1}' | sort -u >> $path/$ts'_ip_blacklist.txt'
+wget -q -O - http://malc0de.com/bl/IP_Blacklist.txt | egrep -v '(^[[:space:]]*/|^[[:space:]]*#|^[[:space:]]*$)|/[0-9]|\:|/g' | awk -F" " '{print $1}' | sort -u >> $path/$ts'_ip_blacklist.txt'
 
 #EmerginThreats
 wget -q -O - http://rules.emergingthreats.net/blockrules/compromised-ips.txt | egrep -v '(^[[:space:]]*/|^[[:space:]]*#|^[[:space:]]*$)|/[0-9]|\:|/g' | awk -F" " '{print $1}' >> $path/$ts'_ip_blacklist.txt'
